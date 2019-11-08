e9a4519d5f3f217e93a21a6501256a0ff171643d
James Robertson <james@jamesrobertson.eu>
2019-11-08 22:45:02 +0000

    Improvement: Removed the gem dependency xml-registry to allow the client to use their own custom built registry.

--- lib/yatter.rb
+++ lib/yatter.rb
@@ -6,7 +6,8 @@
 
 require 'twitter'
 require 'dynarex-password'
-require 'xml-registry'
+
+
 
 class Yatter < Twitter::REST::Client
   
@@ -14,18 +15,61 @@ class Yatter < Twitter::REST::Client
 
     raise 'You must enter a user' if user.nil? 
 
-    e = reg.get_key('hkey_apps/microblog/twitter/' + user)
-    @lookup_file = e.text('lookup_file')
+    key = 'hkey_apps/microblog/twitter/'
+    e = reg.get_key(key + user)
+    @lookup_file = e.text('lookup_file').to_s
     
     super() do |config|                  
       
-      config.consumer_key        = e.text('ctoken')
-      config.consumer_secret     = decipher(e.text('csecret'))
-      config.access_token        = e.text('atoken')
-      config.access_token_secret = decipher(e.text('asecret'))
+      config.consumer_key        = e.text('ctoken').to_s
+      config.consumer_secret     = decipher(e.text('csecret').to_s)
+      config.access_token        = e.text('atoken').to_s
+      config.access_token_secret = decipher(e.text('asecret').to_s)
             
     end
+    
+    if e.text('last_follower').to_s.empty? then
+      reg.set_key(key + 'last_follower', last_follower) 
+    end
 
+  end
+  
+  def followers()
+    
+    @followers ||= super().take(10)
+    
+    @followers.map do |x|
+      
+      OpenStruct.new({
+        name: x.name, 
+        username: x.screen_name, 
+        profile: x.description,        
+        followers: x.followers_count, 
+        following: x.friends_count,
+        member_since: x.created_at, 
+        website: x.website.to_s
+      }).freeze
+      
+    end
+    
+  end
+  
+  def last_follower()
+    
+    @followers ||= followers.take(10)    
+    @followers[0][:username]
+
+  end
+  
+  def mentions()
+    
+    @mentions ||= super()
+    
+    @mentions.map do |x|
+      OpenStruct.new({date: x.created_at, user: x.user.screen_name, 
+                      text: x.text}).freeze
+    end
+    
   end
   
   def update(msg)
--- yatter.gemspec
+++ yatter.gemspec
@@ -1,15 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'yatter'
-  s.version = '0.2.0'
+  s.version = '0.3.0'
   s.summary = 'Yet another Twitter wrapper'
   s.authors = ['James Robertson']
   s.files = Dir['lib/yatter.rb']
-  s.add_runtime_dependency('twitter', '~> 5.15', '>=5.15.0')
-  s.add_runtime_dependency('dynarex-password', '~> 0.1', '>=0.1.11')
-  s.add_runtime_dependency('xml-registry', '~> 0.3', '>=0.3.1')
+  s.add_runtime_dependency('twitter', '~> 6.2', '>=6.2.0')
+  s.add_runtime_dependency('dynarex-password', '~> 0.1', '>=0.1.12')
   s.signing_key = '../privatekeys/yatter.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@r0bertson.co.uk'
+  s.email = 'james@jamesrobertson.eu'
   s.homepage = 'https://github.com/jrobertson/yatter'
 end
